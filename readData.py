7b418213f5da76f9fce3a82b9487ff4a1204bd4a
Benjamen Alford <benjamen.alford@gmail.com>
2022-04-19 22:16:14 -0500

    updated to use dictionary

--- readData.py
+++ readData.py
@@ -3,8 +3,9 @@ import csv
 from requests import head
 
 total_issues = 0
-open_issues = 0
-closed_issues = 0
+issues ={}
+issues['summary']= {}
+issues["total_types"] = 0
 #read my data file
 with open("all_issues.csv") as csvfile:
     issues_reader = csv.reader(csvfile, delimiter=",")
@@ -16,17 +17,28 @@ with open("all_issues.csv") as csvfile:
         #how many open
         #how many closed
         current_status = issue[1]
-        if current_status =="Open":
-            open_issues = open_issues +1
-        elif current_status =="Closed":
-            closed_issues = closed_issues +1
+        if current_status in issues:
+            issues[current_status] = issues[current_status] +1
+        else:
+            issues[current_status] =1
+
+        current_summary = issue[2]
+        if current_summary not in issues['summary']:
+            issues["summary"][current_summary] = 1
+            issues["total_types"] = issues["total_types"] +1
+        else:
+            issues["summary"][current_summary] = issues["summary"][current_summary]  +1
+        # if current_status =="Open":
+        #     open_issues = open_issues +1
+        # elif current_status =="Closed":
+        #     closed_issues = closed_issues +1
         #how many types
 
 
 print(f'Total issues reported: {total_issues}')
-print(f'open issues: {open_issues}')
-print(f'closed issues { closed_issues}')
-
+# print(f'open issues: {open_issues}')
+# print(f'closed issues { closed_issues}')
+print(issues)
 
 
 

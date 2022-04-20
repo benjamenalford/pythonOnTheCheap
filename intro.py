33d5230484450b24c63158c62cb55f116e09ab6a
Benjamen Alford <benjamen.alford@gmail.com>
2022-04-19 21:23:49 -0500

    created intro file

---
+++ intro.py
@@ -0,0 +1,11 @@
+# This is a comment
+age = 41
+name = "Benjamen"
+
+#print multiple variables
+print(age,name)
+
+#print with some format
+print("Hi I am " + name + " I am " + str(age) + " years old ")
+#formatted print
+print(f"Hi I am {name} and I am {age} years old")
